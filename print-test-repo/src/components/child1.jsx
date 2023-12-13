0736a4c46cc55b02e8cc35b2864bcead8d4485e4
colecrum26 <cwcrumrine@gmail.com>
2023-12-12 20:38:52 -0600

    testing printing nested components

--- print-test-repo/src/components/child1.jsx
+++ print-test-repo/src/components/child1.jsx
@@ -1,8 +1,12 @@
-import { forwardRef } from "react"
+import { forwardRef } from "react";
+import MyP from "./child2";
 
 const MyDiv = forwardRef(function MyDiv(props, ref) {
     return (
-        <div ref={ref}>HELLO WORLD</div>
+        <div ref={ref}>
+        <p>HELLO WORLD</p>
+        <MyP />
+        </div>
     );
 });
 
---
+++ print-test-repo/src/components/child2.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react"
+
+// const MyP = forwardRef(function MyP(props, ref) 
+export default function MyP()
+{
+    return (
+        <div>
+        <p>This is some sample text.</p>
+        <p>This is more text.</p>
+        </div>
+    );
+};
+
+// export default MyP;
